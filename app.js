ee6249aa51036c91c9c8b726aae47cd888c29457
Pankaj Pandit <pankajpandit1215@gmail.com>
2023-04-02 19:40:22 +0530

    added template layout, and initial setup.

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-console.log("Hello");
+const express = require("express");
+const path = require("path");
+const fetch = require("node-fetch");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const exphbs = require("express-handlebars");
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const connectDB = require("./config/db");
+
+dotenv.config({ path: "./config/config.env" });
+
+connectDB();
+
+const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+//Handlebars
+app.engine(".hbs", exphbs.engine({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", ".hbs");
+app.set("views", "./views");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", require("./routes/index"));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  res.render("login");
+});
+
+router.get("/home", (req, res, next) => {
+  res.render("home");
+});
+
+module.exports = router;
